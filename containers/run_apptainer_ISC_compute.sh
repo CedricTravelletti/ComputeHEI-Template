35dc4107f6f9478e6d32fde095b0595d299a90cb
CedricTravelletti <cedric.travelletti@hevs.ch>
2025-05-22 10:56:09 +0200

    Updated runscript and container def.

--- containers/run_apptainer_ISC_compute.sh
+++ containers/run_apptainer_ISC_compute.sh
@@ -7,11 +7,12 @@
 #SBATCH --cpus-per-task=1
 
 # Define the location of your code on the host
-export CODE_ON_HOST="/home/cedric/PostPhD/HES/Research/ComputeHEI-Template/"
+# export CODE_ON_HOST="/home/cedric/PostPhD/HES/Research/ComputeHEI-Template/"
+export CODE_ON_HOST="/home/cedric.travelletti/shared_datasets/ComputeHEI-Template/"
 
 # Where the code will be on the container.
 # WARNING: should match the one in the Apptainer.def
 CONTAINER_CODE_LOCATION="/code"
 
 # Run Apptainer, binding the host code directory to the container's /code path
-apptainer run --bind $CODE_ON_HOST:$CONTAINER_CODE_LOCATION my_container.sif
+apptainer run --scratch /tmp --bind $CODE_ON_HOST:$CONTAINER_CODE_LOCATION my_container.sif
